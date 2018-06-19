68ee7a68fb362753b129586d2bee8fc23b05c811
Tarakur Rahman <39719842+tarakurrahman@users.noreply.github.com>
2018-06-19 10:58:20 -0400

    We made a follow cam
    Next step: providing vection and a sense of speed

---
+++ Assets/FollowCam.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class FollowCam : MonoBehaviour {
+	static public GameObject POI;
+
+	[Header("Set in Inspector")]
+	public float easing = 0.05f;
+	public Vector2 minXY = Vector2.zero;
+
+	[Header("Set Dynamically")]
+	public float camZ;
+
+	void Awake(){
+		camZ = this.transform.position.z;
+	}
+	// Use this for initialization
+	void FixedUpdate () {
+		if (POI == null)
+			return;
+		Vector3 destination = POI.transform.position;
+		destination.x = Mathf.Max (minXY.x, destination.x);
+		destination.y = Mathf.Max (minXY.y, destination.y);
+		destination = Vector3.Lerp (transform.position, destination, easing);
+		destination.z = camZ;
+		transform.position = destination;
+		Camera.main.orthographicSize = destination.y + 10;
+	}
+
+	void Start(){
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Slingshot.cs
+++ Assets/Slingshot.cs
@@ -63,6 +63,7 @@ public class Slingshot : MonoBehaviour {
 			aimingMode = false;
 			projectileRigidbody.isKinematic = false;
 			projectileRigidbody.velocity = -mouseDelta * velocityMult;
+			FollowCam.POI = projectile;
 			projectile = null;
 		}
 	}
